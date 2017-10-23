ceda7a51ef218ca7cb743a57121b8c27e2a7f7f6
Dima Kohut <retr0finr1r@gmail.com>
2017-10-23 23:16:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DataKepper
+
+This service will be parsing data from CSV/XLS files, uploading them to the MongoDB and Google Sheets and downloading data from there.
---
+++ src/main/java/com/softbistro/datakeeper/DataKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.softbistro.datakeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataKeeperApplication.class, args);
+	}
+}

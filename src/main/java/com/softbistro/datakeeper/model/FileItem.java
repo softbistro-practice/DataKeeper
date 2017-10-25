f1fe8dd3ad038f23cb7ff63006d665bc3bf4a959
milkho <michael.khoroshun@gmail.com>
2017-10-26 01:54:35 +0300

    Add class that represent a single file

---
+++ src/main/java/com/softbistro/datakeeper/model/FileItem.java
@@ -0,0 +1,52 @@
+package com.softbistro.datakeeper.model;
+
+import java.util.List;
+
+/**
+ * Represent a single file
+ */
+public class FileItem {
+
+	private String name;
+	
+	private String extension;
+	
+	private List<List<DataItem>> dataItems;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public List<List<DataItem>> getDataItems() {
+		return dataItems;
+	}
+
+	public void setDataItems(List<List<DataItem>> dataItems) {
+		this.dataItems = dataItems;
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder()
+				.append("FileItem [name=")
+				.append(this.name)
+				.append(", extension=")
+				.append(this.extension)
+				.append(", dataItems=")
+				.append(this.dataItems)
+				.append("]")
+				.toString();
+	}
+}

88d2563a93581671e34f0d10021b132f7317021a
Michael Khoroshun <michael.khoroshun@gmail.com>
2017-10-27 00:46:20 +0300

    Develop model class that represents a file, issue #6 resolved

---
+++ src/main/java/com/softbistro/datakeeper/model/FileItem.java
@@ -0,0 +1,62 @@
+package com.softbistro.datakeeper.model;
+
+import java.util.List;
+
+/**
+ * Represents a file with it's general info and content
+ */
+public class FileItem {
+
+	private String name;
+	
+	private String extension;
+	
+	private List<List<DataItem>> dataItems;
+
+	public FileItem() {
+		
+	}
+	
+	public FileItem(String name, String extension, List<List<DataItem>> dataItems) {
+		this.name = name;
+		this.extension = extension;
+		this.dataItems = dataItems;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public List<List<DataItem>> getDataItems() {
+		return dataItems;
+	}
+
+	public void setDataItems(List<List<DataItem>> dataItems) {
+		this.dataItems = dataItems;
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder()
+				.append("FileItem [name=")
+				.append(this.name)
+				.append(", extension=")
+				.append(this.extension)
+				.append(", dataItems=")
+				.append(this.dataItems)
+				.append("]")
+				.toString();
+	}
+}

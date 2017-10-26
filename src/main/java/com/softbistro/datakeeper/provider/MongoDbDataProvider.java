dedfac12c58aa3292b9733ea123883c19a3fb13d
Max Legelbakh <maximusfk@gmail.com>
2017-10-26 21:57:31 +0300

    Added DataProvider implementation for MongoDB #1

---
+++ src/main/java/com/softbistro/datakeeper/provider/MongoDbDataProvider.java
@@ -0,0 +1,14 @@
+package com.softbistro.datakeeper.provider;
+
+import java.util.List;
+
+import com.softbistro.datakeeper.model.DataItem;
+
+public class MongoDbDataProvider implements IDataProvider {
+
+	@Override
+	public List<List<String>> getDataRows() {
+		return null;
+	}
+
+}

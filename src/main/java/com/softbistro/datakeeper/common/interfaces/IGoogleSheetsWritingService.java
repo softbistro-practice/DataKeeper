861890c7ecb584adbb8e32b0c8218baf650efd35
Dima Kohut <retr0finr1r@gmail.com>
2017-10-26 23:38:01 +0300

    Interface with methods for data writing into Google Sheets was created, #7

---
+++ src/main/java/com/softbistro/datakeeper/common/interfaces/IGoogleSheetsWritingService.java
@@ -0,0 +1,23 @@
+package com.softbistro.datakeeper.common.interfaces;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import com.softbistro.datakeeper.model.DataItem;
+
+/**
+ * 
+ * @author Dmytro Kohut
+ *
+ */
+public interface IGoogleSheetsWritingService {
+
+	/**
+	 * Insert data into Google Sheets.
+	 * 
+	 * @throws GeneralSecurityException 
+	 * @throws IOException 
+	 */
+	boolean writeSheet(List<List<DataItem>> data, String filename) throws IOException, GeneralSecurityException;
+}

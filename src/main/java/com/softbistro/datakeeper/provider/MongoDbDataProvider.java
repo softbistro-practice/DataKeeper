9401bc4d08166d331abb2227f6a55a9588463430
Max Legelbakh <maximusfk@gmail.com>
2017-10-27 10:59:38 +0300

    Defined method for loading data from mongodb documents

--- src/main/java/com/softbistro/datakeeper/provider/MongoDbDataProvider.java
+++ src/main/java/com/softbistro/datakeeper/provider/MongoDbDataProvider.java
@@ -1,14 +1,36 @@
 package com.softbistro.datakeeper.provider;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.softbistro.datakeeper.model.DataItem;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MongoDbDataProvider implements IDataProvider {
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	@Override
 	public List<List<String>> getDataRows() {
-		return null;
+		List<List<String>> result = new ArrayList<>();
+		for(String collectionName : this.mongoTemplate.getCollectionNames()) {
+			for(Document document : this.mongoTemplate.getCollection(collectionName).find()) {
+				result.add(this.toStringList(document));
+			}
+		}
+		
+		return result;
 	}
-
+	
+	private List<String> toStringList(Document document) {
+		List<String> result = new ArrayList<>();
+		document.forEach((key, value) -> {
+			result.add(value.toString());
+		});
+		return result;
+	}
 }

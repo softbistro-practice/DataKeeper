c522cebd480e70100100369933999dd018c5cc2b
Max Legelbakh <maximusfk@gmail.com>
2017-10-27 09:35:48 +0300

    Added javadoc for IDataProvider #1

--- src/main/java/com/softbistro/datakeeper/provider/IDataProvider.java
+++ src/main/java/com/softbistro/datakeeper/provider/IDataProvider.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface IDataProvider {
 
+	/**
+	 * Return all rows data from container
+	 * @return List rows in String
+	 */
 	List<List<String>> getDataRows();
 	
 }

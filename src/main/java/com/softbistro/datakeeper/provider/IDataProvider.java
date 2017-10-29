7d7defdec2c32a21a46f3715c006129068084af8
Max Legelbakh <maximusfk@gmail.com>
2017-10-29 08:45:13 +0200

    Edited javadoc in IDataProvider

--- src/main/java/com/softbistro/datakeeper/provider/IDataProvider.java
+++ src/main/java/com/softbistro/datakeeper/provider/IDataProvider.java
@@ -7,7 +7,7 @@ public interface IDataProvider {
 	/**
 	 * Return all rows data from container with specified collection name
 	 * 
-	 * @param collectionName Name of data collection in mongodb
+	 * @param collectionName Name of data collection
 	 * @return List rows in String
 	 */
 	List<List<String>> getDataRows(String collectionName);
